e47e968f1003a9f6cf2c59eced21a7a437e01d89
irfanalam05 <mohdirfanalam77886@gmail.com>
2025-12-16 22:27:09 +0530

    Time: 18 ms (59.51%), Space: 101.1 MB (89.57%) - LeetHub

---
+++ 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long minOperations(int[][] queries) {
+        long total = 0;
+        for (int[] query : queries) {
+            int l = query[0], r = query[1];
+            total += (getOperations(r) - getOperations(l - 1) + 1) / 2;
+        }
+        return total;
+    }
+
+    private long getOperations(int n) {
+        long res = 0;
+        int ops = 0;
+        for (int powerOfFour = 1; powerOfFour <= n; powerOfFour *= 4) {
+            int left = powerOfFour;
+            int right = Math.min(n, powerOfFour * 4 - 1);
+            res += (long)(right - left + 1) * ++ops;
+        }
+        return res;
+    }
+}
