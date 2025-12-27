6e635a268cb803ff0604d774608384b6acb52641
irfanalam05 <mohdirfanalam77886@gmail.com>
2025-12-27 22:58:37 +0530

    Time: 11 ms (90.01%), Space: 88.3 MB (72.19%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        int n = nums.length;
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            minVal = Math.min(minVal, num);
+            maxVal = Math.max(maxVal, num);
+        }
+
+        if (minVal == maxVal) {
+            return 0;
+        }
+
+        int bucketSize = Math.max(1, (maxVal - minVal) / (n - 1));
+        int bucketCount = (maxVal - minVal) / bucketSize + 1;
+
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+
+        Arrays.fill(bucketMin, Integer.MAX_VALUE);
+        Arrays.fill(bucketMax, Integer.MIN_VALUE);
+
+        for (int num : nums) {
+            int idx = (num - minVal) / bucketSize;
+            bucketMin[idx] = Math.min(bucketMin[idx], num);
+            bucketMax[idx] = Math.max(bucketMax[idx], num);
+        }
+
+        int maxGap = 0;
+        int prevMax = minVal;
+
+        for (int i = 0; i < bucketCount; i++) {
+            if (bucketMin[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+            maxGap = Math.max(maxGap, bucketMin[i] - prevMax);
+            prevMax = bucketMax[i];
+        }
+        return maxGap;
+    }
+}
