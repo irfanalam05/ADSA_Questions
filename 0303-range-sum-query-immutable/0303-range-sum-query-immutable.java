41badf49e40ee57d01ec2a5a9d1850afba60e1b4
irfanalam05 <mohdirfanalam77886@gmail.com>
2026-02-17 14:35:31 +0530

    Time: 8 ms (71.83%), Space: 47.7 MB (82.27%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -0,0 +1,27 @@
+class NumArray {
+    private int[] sumArray;  // yaha cumulative sums store honge
+
+    public NumArray(int[] nums) {
+        sumArray = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            sumArray[i] = sum;
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0) {
+            return sumArray[right];
+        } else {
+            return sumArray[right] - sumArray[left - 1];
+        }
+    }
+}
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
