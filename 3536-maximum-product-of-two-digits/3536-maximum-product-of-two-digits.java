5d401595ab90cf6e3faa8536a42b72ae84b76d56
MOHD IRFAN ALAM <187972652+irfanalam05@users.noreply.github.com>
2026-07-25 20:46:18 +0530

    Time: 1 ms (100%), Space: 42.7 MB (58.74%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int n) {
+        int first=0;
+        int sec=0;
+        while(n !=0){
+            int dig= n% 10;
+            if(dig>= first){
+                sec=first;
+                first=dig;
+            }
+            else if(dig>sec){
+                sec=dig;
+            }
+            n=n/10;
+        }
+        return first* sec;
+    }
+}
